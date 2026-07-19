1679ddb304d9942a22efd057cc852c6ba87616a0
Shijie Chen <2635836894@qq.com>
2026-07-19 23:34:27 +0800

    delete useless files; update ac automation;

--- src/string/AC自动机.cpp
+++ src/string/AC自动机.cpp
@@ -1,119 +1,71 @@
 struct AhoCorasick {
     static constexpr int ALPHABET = 26;
     struct Node {
-        int len;
-        int link;
+        int len;   // 此处的字符串长度
+        int link;  // 失效指针
+        int old;   // 该节点对应的下一个
+        int num;   // 该节点是哪一个模式串的结尾
         array<int,ALPHABET> next;
-        Node(): len{0}, link{0}, next{} { }
     };
-
     vector<Node> t;
-
-    AhoCorasick() {
-        init();
-    }
-
-    void init() {
-        t.assign(2, Node());
+    AhoCorasick(): t(2, Node()) {
         t[0].next.fill(1);
         t[0].len = -1;
     }
-
     int newNode() {
         t.emplace_back();
         return t.size() - 1;
     }
-
-    int add(const string &a) {
+    int add(const string &a, int dir) {
         int p = 1;
         for (auto c: a) {
-            int x = c - 'a';
+            int x = c-'a';
             if (t[p].next[x] == 0) {
                 t[p].next[x] = newNode();
                 t[t[p].next[x]].len = t[p].len + 1;
             }
             p = t[p].next[x];
         }
+        t[p].num = dir;
         return p;
     }
-
-    void work() {
+    void build() {
         queue<int> q;
         q.push(1);
-
         while (!q.empty()) {
-            int x = q.front();
-            q.pop();
-
+            int x = q.front(); q.pop();
             for (int i = 0; i < ALPHABET; ++i) {
-                if (t[x].next[i] == 0) {
-                    t[x].next[i] = t[t[x].link].next[i];
+                int &u = t[x].link;
+                int &v = t[x].next[i];
+                if (v == 0) {
+                    v = t[u].next[i];
                 } else {
-                    t[t[x].next[i]].link = t[t[x].link].next[i];
-                    q.push(t[x].next[i]);
+                    t[v].link = t[u].next[i];
+                    t[v].old = t[t[v].link].num ? t[v].link : t[t[v].link].old;
+                    q.push(v);
                 }
             }
         }
     }
-
-    int next(int p, int x) {
-        return t[p].next[x];
-    }
-
-    int link(int p) {
-        return t[p].link;
-    }
-
-    int len(int p) {
-        return t[p].len;
-    }
-
-    int size() {
-        return t.size();
-    }
+    int next(int p, int x) { return t[p].next[x]; }
+    int link(int p) { return t[p].link; }
+    int old(int p) { return t[p].old; }
+    int num(int p) { return t[p].num; }
+    int size() { return t.size(); }
 };
 
-constexpr int N = 2e6+5;
-vector<int> G[N];
-int cnt[N];
-
-void dfs(int u) 
-{
-    for (auto v: G[u]) {
-        dfs(v);
-        cnt[u] += cnt[v];
-    }
-}
-
-void solve()
-{
-	int n;
-	cin >> n;
-
-    AhoCorasick AC;
-    vector<int> pos(n + 1);
-    for (int i = 1; i <= n; ++i) {
-        string s;
-        cin >> s;
-        pos[i] = AC.add(s);
-    }
-    AC.work();
-
-    string T;
-    cin >> T;
+void solve() {
+    ac.add();  // 插入若干模式串
+    ac.build();
+    string s; cin >> s;
     int p = 1;
-    for (int i = 0; i < int(T.size()); ++i) {
-        int x = T[i] - 'a';
-        p = AC.next(p, x);
-        ++cnt[p];
-    }
-
-    for (int i = 1; i < AC.size(); ++i) {
-        G[AC.link(i)].push_back(i);
-    }
-    dfs(1);
-
     for (int i = 1; i <= n; ++i) {
-        cout << cnt[pos[i]] << "\n";
+        int x = s[i-1] - 'a';
+        p = ac.next(p, x);
+        for (int j = p; j; j=ac.old(j)) {
+            if (ac.num(j)) {
+                // ...
+            }
+        }
     }
 }
